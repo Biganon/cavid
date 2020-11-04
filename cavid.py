13047561d7f9d5596fc8157e860d3e09e00e0f81
Simon Junod <sj@simonjunod.ch>
2020-11-04 21:07:48 +0100

    Ajout du paramètre max_muxing_queue_size à 1024 partout

--- cavid.py
+++ cavid.py
@@ -3,7 +3,10 @@ import os
 import subprocess
 import re
 import pyperclip
+import multiprocessing
+import time
 from termcolor import colored
+from datetime import datetime
 
 original = sys.argv[1]
 threshold = 0.3
@@ -16,13 +19,14 @@ LOG_SUCCESS = 3
 LOG_INPUT = 4
 
 def log(string, level, *args, **kwargs):
+    now = datetime.now().strftime("%H:%M:%S")
     color = ("white", "yellow", "red", "green", "blue")[level]
     code = ("I", "W", "E", "S", "?")[level]
-    print(colored(f"[{code}] {string}", color), *args, **kwargs)
+    print(colored(f"[{now}] [{code}] {string}", color), *args, **kwargs)
 
 def check_or_die(cp):
     if cp.returncode != 0:
-        log("La dernière commande s'est terminée avec un code de retour non nul. Arrêt.", LOG_ERROR)
+        log(f"La dernière commande s'est terminée avec un code de retour non nul. Arrêt. {cp.stderr}", LOG_ERROR)
         sys.exit()
 
 log(f"Commence l'extraction des timecodes de changements de scène (seuil = {threshold})...", LOG_INFO)
@@ -36,6 +40,7 @@ timecodes = re.findall(r"pts_time:([0-9.]+)", cp.stderr.decode("utf-8"))
 log(f"Extraction terminée, {len(timecodes)} timecodes trouvés.", LOG_SUCCESS)
 log(f"Commence la création des extraits...", LOG_INFO)
 
+a = time.time()
 for i in range(len(timecodes)-1):
     start = timecodes[i]
     end = timecodes[i+1]
@@ -44,6 +49,7 @@ for i in range(len(timecodes)-1):
                         capture_output=True)
     check_or_die(cp)
     log(f"Extrait {i+1}/{len(timecodes)-1} créé.", LOG_SUCCESS)
+print(time.time() - a)
 
 log(f"Ouvrir un navigateur de fichiers (Nautilus ou Thunar conseillés), sélectionner tous les extraits à laisser intouchés, puis copier dans le presse-papiers (ctrl+c).", LOG_INFO)
 
@@ -67,23 +73,25 @@ while True:
 extracts = sorted([f for f in os.listdir() if re.match(r"[0-9]+\.mp4", f) and f not in untouched])
 log(f"{len(extracts)} extraits à caviarder...", LOG_INFO)
 log(f"Commence le caviardage...", LOG_INFO)
+a = time.time()
 for idx, extract in enumerate(extracts):
     cp = subprocess.run(["ffprobe", "-show_streams", extract], capture_output=True)
     check_or_die(cp)
 
     has_video = bool(re.search(r"codec_type=video", cp.stdout.decode("utf-8")))
     if has_video:
-        cp = subprocess.run(["ffmpeg", "-i", extract, "-i", mask, "-filter_complex", "[1][0]scale2ref[i][v];[v][i]overlay", "-c:a", "copy", f"h{extract}"],
+        cp = subprocess.run(["ffmpeg", "-i", extract, "-i", mask, "-filter_complex", "[1][0]scale2ref[i][v];[v][i]overlay", "-c:a", "copy", "-max_muxing_queue_size", "1024", f"h{extract}"],
                             capture_output=True)
         check_or_die(cp)
 
         log(f"Extrait {idx+1}/{len(extracts)} caviardé.", LOG_SUCCESS)
     else:
-        cp = subprocess.run(["ffmpeg", "-i", extract, "-i", mask, "-acodec", "copy", "-vcodec", "h264", f"h{extract}"],
+        cp = subprocess.run(["ffmpeg", "-i", extract, "-i", mask, "-acodec", "copy", "-vcodec", "h264", "-max_muxing_queue_size", "1024", f"h{extract}"],
                             capture_output=True)
         check_or_die(cp)
 
         log(f"Extrait {idx+1}/{len(extracts)} caviardé (le flux vidéo n'existait pas, et a été créé).", LOG_WARNING)
+print(time.time() - a)
 
 log("Renomme les extraits caviardés...", LOG_INFO)
 for extract in extracts:
@@ -93,12 +101,14 @@ log("Extraits renommés.", LOG_SUCCESS)
 extracts = sorted([f for f in os.listdir() if re.match(r"[0-9]+\.mp4", f)])
 log(f"{len(extracts)} à sanitiser...", LOG_INFO)
 
+a = time.time()
 for idx, extract in enumerate(extracts):
     cp = subprocess.run(["ffmpeg", "-i", extract, "-q", "0", "-max_muxing_queue_size", "1024", f"s{extract}"],
                         capture_output=True)
     check_or_die(cp)
 
     log(f"Extrait {idx+1}/{len(extracts)} sanitisé.", LOG_SUCCESS)
+print(time.time() - a)
 
 log("Renomme les extraits sanitisés...", LOG_INFO)
 for extract in extracts:
