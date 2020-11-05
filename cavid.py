a07cc131f903445df4e8439bacb12b0bff21aadc
Simon Junod <sj@simonjunod.ch>
2020-11-05 01:56:30 +0100

    Parallélisation terminée

--- cavid.py
+++ cavid.py
@@ -3,10 +3,11 @@ import os
 import subprocess
 import re
 import pyperclip
-import multiprocessing
 import time
 from termcolor import colored
 from datetime import datetime
+from multiprocessing import Pool, Value, Lock
+from ctypes import c_int
 
 original = sys.argv[1]
 threshold = 0.3
@@ -29,27 +30,49 @@ def check_or_die(cp):
         log(f"La dernière commande s'est terminée avec un code de retour non nul. Arrêt. {cp.stderr}", LOG_ERROR)
         sys.exit()
 
+### DECOUPAGE
+
 log(f"Commence l'extraction des timecodes de changements de scène (seuil = {threshold})...", LOG_INFO)
 cp = subprocess.run(["ffmpeg", "-i", original, "-filter:v", "select='gt(scene,0.3)',showinfo", "-f", "null", "-"],
-                    capture_output=True)
+                    capture_output=True, stdin=subprocess.DEVNULL)
 
 check_or_die(cp)
 
 timecodes = re.findall(r"pts_time:([0-9.]+)", cp.stderr.decode("utf-8"))
 
-log(f"Extraction terminée, {len(timecodes)} timecodes trouvés.", LOG_SUCCESS)
+cp = subprocess.run(["ffprobe", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", original],
+                    capture_output=True, stdin=subprocess.DEVNULL)
+check_or_die(cp)
+
+start = "0.0"
+end = cp.stdout.decode("utf-8").strip()
+timecodes.insert(0, start)
+timecodes.append(end)
+
+log(f"Extraction terminée.", LOG_SUCCESS)
 log(f"Commence la création des extraits...", LOG_INFO)
 
-a = time.time()
+counter = Value(c_int)
+counter_lock = Lock()
+def create_extract(original, start, end, index):
+    filename = str(index).zfill(3) + ".mp4"
+    cp = subprocess.run(["ffmpeg", "-i", original, "-ss", start, "-to", end, "-c", "copy", filename],
+                        capture_output=True, stdin=subprocess.DEVNULL)
+    check_or_die(cp)
+    with counter_lock:
+        counter.value += 1
+        log(f"Extrait créé ({counter.value}/{len(timecodes)-1})", LOG_SUCCESS)    
+
+parameters = []
 for i in range(len(timecodes)-1):
     start = timecodes[i]
     end = timecodes[i+1]
-    filename = str(i+1).zfill(3) + ".mp4"
-    cp = subprocess.run(["ffmpeg", "-i", original, "-ss", start, "-to", end, "-c", "copy", filename],
-                        capture_output=True)
-    check_or_die(cp)
-    log(f"Extrait {i+1}/{len(timecodes)-1} créé.", LOG_SUCCESS)
-print(time.time() - a)
+    index = i+1
+    parameters.append((original, start, end, index))
+with Pool() as pool:
+    pool.starmap(create_extract, parameters)
+
+### TRI
 
 log(f"Ouvrir un navigateur de fichiers (Nautilus ou Thunar conseillés), sélectionner tous les extraits à laisser intouchés, puis copier dans le presse-papiers (ctrl+c).", LOG_INFO)
 
@@ -70,50 +93,62 @@ while True:
     else:
         continue
 
+### CAVIARDAGE
+
 extracts = sorted([f for f in os.listdir() if re.match(r"[0-9]+\.mp4", f) and f not in untouched])
-log(f"{len(extracts)} extraits à caviarder...", LOG_INFO)
-log(f"Commence le caviardage...", LOG_INFO)
-a = time.time()
-for idx, extract in enumerate(extracts):
-    cp = subprocess.run(["ffprobe", "-show_streams", extract], capture_output=True)
+log(f"Commence le caviardage de {len(extracts)} extraits...", LOG_INFO)
+
+counter = Value(c_int)
+counter_lock = Lock()
+def redact_extract(extract):
+    cp = subprocess.run(["ffprobe", "-show_streams", extract], capture_output=True, stdin=subprocess.DEVNULL)
     check_or_die(cp)
 
     has_video = bool(re.search(r"codec_type=video", cp.stdout.decode("utf-8")))
+
     if has_video:
         cp = subprocess.run(["ffmpeg", "-i", extract, "-i", mask, "-filter_complex", "[1][0]scale2ref[i][v];[v][i]overlay", "-c:a", "copy", "-max_muxing_queue_size", "1024", f"h{extract}"],
-                            capture_output=True)
-        check_or_die(cp)
-
-        log(f"Extrait {idx+1}/{len(extracts)} caviardé.", LOG_SUCCESS)
+                            capture_output=True, stdin=subprocess.DEVNULL)
     else:
         cp = subprocess.run(["ffmpeg", "-i", extract, "-i", mask, "-acodec", "copy", "-vcodec", "h264", "-max_muxing_queue_size", "1024", f"h{extract}"],
-                            capture_output=True)
-        check_or_die(cp)
+                            capture_output=True, stdin=subprocess.DEVNULL)
+    check_or_die(cp)
+    with counter_lock:
+        counter.value += 1
+        log(f"Extrait caviardé ({counter.value}/{len(extracts)})", LOG_SUCCESS)
 
-        log(f"Extrait {idx+1}/{len(extracts)} caviardé (le flux vidéo n'existait pas, et a été créé).", LOG_WARNING)
-print(time.time() - a)
+with Pool() as pool:
+    pool.map(redact_extract, extracts)
 
 log("Renomme les extraits caviardés...", LOG_INFO)
 for extract in extracts:
     os.rename(f"h{extract}", extract)
 log("Extraits renommés.", LOG_SUCCESS)
 
-extracts = sorted([f for f in os.listdir() if re.match(r"[0-9]+\.mp4", f)])
-log(f"{len(extracts)} à sanitiser...", LOG_INFO)
+### NORMALISATION
 
-a = time.time()
-for idx, extract in enumerate(extracts):
+extracts = sorted([f for f in os.listdir() if re.match(r"[0-9]+\.mp4", f)])
+log(f"Commence la normalisation de {len(extracts)} extraits...", LOG_INFO)
+counter = Value(c_int)
+counter_lock = Lock()
+def normalize_extract(extract):
     cp = subprocess.run(["ffmpeg", "-i", extract, "-q", "0", "-max_muxing_queue_size", "1024", f"s{extract}"],
-                        capture_output=True)
+                        capture_output=True, stdin=subprocess.DEVNULL)
     check_or_die(cp)
 
-    log(f"Extrait {idx+1}/{len(extracts)} sanitisé.", LOG_SUCCESS)
-print(time.time() - a)
+    with counter_lock:
+        counter.value += 1
+        log(f"Extrait normalisé ({counter.value}/{len(extracts)})", LOG_SUCCESS)
 
-log("Renomme les extraits sanitisés...", LOG_INFO)
+with Pool() as pool:
+    pool.map(normalize_extract, extracts)
+
+log("Renomme les extraits normalisés...", LOG_INFO)
 for extract in extracts:
     os.rename(f"s{extract}", extract)
 log("Extraits renommés.", LOG_SUCCESS)
+
+### CONCATÉNATION
 
 log("Commence la concaténation finale...", LOG_INFO)
 concat_file = ""
@@ -123,9 +158,11 @@ for extract in sorted(extracts):
 with open("concat_file.txt", "w") as f:
     f.write(concat_file)
 
-cp = subprocess.run(["ffmpeg", "-f", "concat", "-i", "concat_file.txt", "-c", "copy", "final.mp4"], capture_output=True)
+cp = subprocess.run(["ffmpeg", "-f", "concat", "-i", "concat_file.txt", "-c", "copy", "final.mp4"], capture_output=True, stdin=subprocess.DEVNULL)
 check_or_die(cp)
 log("Concaténation terminée.", LOG_SUCCESS)
+
+### NETTOYAGE
 
 log("Supprime les extraits et le fichier de concaténation...", LOG_INFO)
 for extract in extracts:
